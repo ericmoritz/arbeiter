96a3af916679cd51a9670fc4abfc7841c75ef268
Eric Moritz <eric@themoritzfamily.com>
2011-11-19 23:11:19 -0500

    Added my standard BSD license

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ version = '0.1'
 
 setup(name='arbeiter',
       version=version,
-      description="A really simple realtime work queue system",
+      description="An unassuming work queue system",
       long_description="""\
-A really simple work queue system built using Kestrel""",
+A work queue system built using Kestrel""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Eric Moritz',
       author_email='eric@themoritzfamily.com',
       url='',
-      license='',
+      license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
